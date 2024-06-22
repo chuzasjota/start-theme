a14605634ad629950d0edae7c626c508738057ec
Leonardo Bravo <leo.bravo.revelo@gmail.com>
2024-06-21 23:39:08 -0500

    🎉 Initial commit

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The template for displaying the front page
+ *
+ * This is the template that displays the front page of your WordPress site.
+ * You can use this template to customize the layout and content of your front page.
+ */
+
+get_header(); ?>
+
+<main class="front-page">
+
+</main>
+
+<?php get_footer();
